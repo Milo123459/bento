4b44457b8b1dfdd89cddae180b363ef79d102995
Milo <50248166+Milo123459@users.noreply.github.com>
2021-04-17 11:14:08 +0100

    lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-
-  return (
-      <div className="bg-primary">
-  <Component {...pageProps} />
-  </div>
-  )
+	return (
+		<div className="bg-primary">
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import Head from 'next/head';
-import { GearIcon } from '@primer/octicons-react'; 
+import { GearIcon } from '@primer/octicons-react';
 
 export default function Home() {
-  return (
-      <div>
-          <div className="pr-4 pt-3 bg-primary">
-              <GearIcon className="float-right" size={32} />
-          </div>
-      </div>
-  )
+	return (
+		<div>
+			<div className="pr-4 pt-3 bg-primary">
+				<GearIcon className="float-right hover:animate-spin" size={32} />
+			</div>
+		</div>
+	);
 }
