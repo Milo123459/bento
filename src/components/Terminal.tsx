7164f5d4067d656bcdb29fb700cccfbf51116806
Fionn <f@fnionn.com>
2021-04-24 11:51:41 +0100

    feat(ACTUALLYWORKING): Fixed Terminal!

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -5,7 +5,19 @@ function TerminalComponent() {
   useEffect(() => {
     var term = new Terminal();
     term.open(document.getElementById("terminal"));
-    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+    term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $");
+    var shellprompt = '$ ';
+    const prompt = function () {
+    term.write('\r\n' + shellprompt);
+    };
+    term.onKey((key) => {
+      console.log(key.domEvent.key)
+      if(key.key == "\r") {
+        return prompt()
+      }
+      console.log(key.key)
+      term.write(key.key)
+    })
   }, []);
 
   return <div id="terminal" />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,7 @@ import dynamic from "next/dynamic";
 const DynamicTerminal = dynamic(() => import("../components/Terminal"), {
   ssr: false
 });
-
-import("../components/Terminal").then(z => z.default()).on('key', (key, ev) => {
-	console.log(key.charCodeAt(0));
-	if (key.charCodeAt(0) == 13)
-		term.write('\n');
-	term.write(key);
-});
+ 
 
 export default function IndexPage() {
   return <DynamicTerminal />;
