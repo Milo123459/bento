5b76f2ecc8b2fd411625bb17339633360dab5f76
Milo <50248166+Milo123459@users.noreply.github.com>
2021-04-29 08:00:32 +0100

    chore(Style): run fmt

--- build.rs
+++ build.rs
@@ -4,12 +4,12 @@ use std::fs::File;
 use std::path::PathBuf;
 
 fn main() {
-    let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
+	let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
 
-    println!("cargo:rerun-if-changed=build.rs");
+	println!("cargo:rerun-if-changed=build.rs");
 
-    let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
-    Registry::new(Api::Gles2, (3, 3), Profile::Core, Fallbacks::All, [])
-        .write_bindings(gl_generator::StructGenerator, &mut file)
-        .unwrap();
+	let mut file = File::create(&dest.join("gl_bindings.rs")).unwrap();
+	Registry::new(Api::Gles2, (3, 3), Profile::Core, Fallbacks::All, [])
+		.write_bindings(gl_generator::StructGenerator, &mut file)
+		.unwrap();
 }
